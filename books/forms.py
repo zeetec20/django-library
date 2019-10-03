2dd3c13dd8202cef56d3989ac381ae8f8989eb66
Firman Justisio Lestari <jusles363@gmail.com>
2019-10-03 10:33:05 +0700

    done update Books

--- books/forms.py
+++ books/forms.py
@@ -1,2 +1,54 @@
 from django import forms
+from .models import Book
 
+class BookForm(forms.ModelForm):
+    
+    class Meta:
+        model = Book
+        fields = (
+            "title",
+            "author",
+            "genre",
+            "publishDate",
+            "pages",
+            "price"
+        )
+
+        widgets = {
+            'title': forms.TextInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputTitle',
+                    'name': 'title'
+                }
+            ),
+            'author': forms.TextInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputAuthor',
+                    'name': 'author'
+                }
+            ),
+            'genre': forms.TextInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputGenre',
+                    'name': 'genre'
+                }
+            ),
+            'publishDate': forms.DateInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputPublishDate',
+                    'name': 'publishDate'
+                }
+            ),
+            'pages': forms.NumberInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputPages',
+                    'name': 'pages'
+                }
+            ),
+            'price': forms.NumberInput(
+                attrs = {
+                    'class': 'form-control form-control-sm inputPrice',
+                    'name': 'price'
+                }
+            )
+        }
--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
 import datetime
+from django.http import JsonResponse
+import copy
 
 def index(request):
     context = {
@@ -19,13 +22,19 @@ def actionBooks(request):
             if request.GET['action'] == 'update':
                 number = (int(request.GET['book']) - 1)
                 book = Book.objects.all()[number]
-                date = book.publishDate
+                valueBook = book.__dict__
+                print(valueBook)
+                form = BookForm(initial = valueBook)
+                date = copy.deepcopy(book.publishDate)
                 print(date)
+
                 context = {
                     'book': Book.objects.all()[number],
                     'bookPublishDate': date,
+                    'formBook': form,
                     'bookNumber': int(request.GET['book'])
                 }
+
                 return render(request, 'update-add-book.html', context)
             elif request.GET['action'] == 'create':
                 return render(request, 'update-add-book.html', {})
@@ -33,6 +42,16 @@ def actionBooks(request):
 def saveBooks(request, action):
     if request.method == 'POST':
         if action == 'update':
-            pass
+            number = (int(request.POST['number']) - 1)
+            book = Book.objects.all()[number],
+            book = book[0]
+            book.title = request.POST['title']
+            book.author = request.POST['author']
+            book.genre = request.POST['genre']
+            book.publishDate = request.POST['publishDate']
+            book.pages = request.POST['pages']
+            book.price = request.POST['price']
+            book.save()
+            return JsonResponse({'success': True})
         elif action == 'create':
             pass
